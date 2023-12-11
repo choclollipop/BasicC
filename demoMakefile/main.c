39afd4bb5516db61b7434761ef8a120f0b66133f
choclollipop <2924733894@qq.com>
2023-12-11 20:12:47 +0800

    [新增] 新增makefile写法

--- demoMakefile/main.c
+++ demoMakefile/main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 
-int printString()
-{
-    int ret = 0;
-    printf("print string\n");
-    return 0;
-}
-
 int main()
 {
-    int num1 = 10;
-    printf("num1:%d\n", num1);
-    printString();
+    printf("hello world\n");
 
     return 0;
 }
