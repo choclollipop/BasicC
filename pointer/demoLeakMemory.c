647ca0c3b8b4a3d942c9d4993a0e189ba23de601
choclollipop <2924733894@qq.com>
2023-12-13 16:52:16 +0800

    [新增] 新增内存泄漏问题

--- pointer/demoLeakMemory.c
+++ pointer/demoLeakMemory.c
@@ -1,8 +1,15 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BUFFER_SIZE 10
 
-/* 内存泄漏 */
+/* 内存泄漏 
+    1.野指针
+    2.malloc分配的堆空间，没有被释放
+    3.踩内存
+
+*/
 
 int main()
 {
@@ -19,8 +26,37 @@ int main()
     }
 #endif
 
-    char * p;
-    printf("\n");
+    // char * p;
+    // printf("\n");
+
+    //case 2:
+    char * ptr = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+    if(!ptr)
+    {
+        /* todo... */
+        /* 状态码 */
+        return -1;
+    }
+    /* 使用malloc分配的空间是脏数据 */
+
+    memset(ptr, 0, sizeof(char) * BUFFER_SIZE);
+
+#if 0
+    /* 极度危险的函数, 改用strnpy */
+    strcpy(ptr, "hello");
+
+#endif
+    /* 使用安全 （记得加结束符，所以需要空间减1，存放结束符）*/
+    strncpy(ptr, "hello worldaaaa", BUFFER_SIZE - 1);
+    printf("ptr:%s\n", ptr);
+
+    /* 释放指针函数 */
+    if(ptr)
+    {
+        free(ptr);
+        ptr = NULL;
+    }
+
 
     return 0;
 }
